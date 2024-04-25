6ea2a192bc677bb535859b748c910aa64a299c54
John Doe <lucashaleluia@hotmail.com>
2024-04-25 14:08:15 -0300

    exercises 1-6 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if(num1 > num2){
+        for(let i = num1; i >= num2; i--){
+            sum += i;
+
+        }
+    }else{
+        for(let i = num1; i <= num2; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
+
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let x = false
+
+    if(year % 4 == 0){
+        x = true;
+        if(year % 100 == 0){
+            x = false;
+            if(year % 400 == 0){
+                x = true;
+            }
+        }
+    }
+
+    return x;
 
 };
 
 // Do not edit below this line
+/* Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900)
+ unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing) */
 module.exports = leapYears;
